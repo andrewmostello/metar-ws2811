6c5dd033e97735c6b6561d9e01aa8f187ace4d2e
Andrew Mostello <amostello@expertek.com>
2024-04-14 14:22:39 -0400

    add ws2811 controller

--- ws2811/ws2811.go
+++ ws2811/ws2811.go
@@ -1,77 +1,143 @@
 package light
 
 import (
-	"math/rand"
-	"time"
+	"context"
+	"fmt"
+	"log/slog"
 
+	"github.com/andrewmostello/metar-ws2811/metar"
 	ws281x "github.com/rpi-ws281x/rpi-ws281x-go"
 )
 
 const (
-	brightness = 128
-	ledCounts  = 300
-	gpioPin    = 18
-	freq       = 800000
-	sleepTime  = 200
+	DefaultBrightness = 128
+	DefaultLEDCounts  = 300
+	DefaultGPIOPin    = 18
 )
 
-type ws struct {
-	ws2811 *ws281x.WS2811
+var (
+	DefaultColors = map[metar.FlightCategory]RGB{
+		metar.FlightCategoryUnknown: RGB{
+			Red:   0,
+			Green: 0,
+			Blue:  0,
+		},
+		metar.FlightCategoryVFR: RGB{
+			Red:   0,
+			Green: 255,
+			Blue:  0,
+		},
+		metar.FlightCategoryMVFR: RGB{
+			Red:   0,
+			Green: 0,
+			Blue:  255,
+		},
+		metar.FlightCategoryIFR: RGB{
+			Red:   255,
+			Green: 0,
+			Blue:  0,
+		},
+		metar.FlightCategoryLIFR: RGB{
+			Red:   125,
+			Green: 0,
+			Blue:  125,
+		},
+	}
+)
+
+type RGB struct {
+	Red   int
+	Green int
+	Blue  int
 }
 
-func (ws *ws) init() error {
-	err := ws.ws2811.Init()
-	if err != nil {
+func (rgb RGB) ToColor() uint32 {
+	return uint32(uint32(rgb.Red)<<16 | uint32(rgb.Green)<<8 | uint32(rgb.Blue))
+}
+
+type Option func(*ws281x.ChannelOption)
+
+type Controller struct {
+	Logger *slog.Logger
+	Colors map[metar.FlightCategory]RGB
+}
+
+func RGBToColor(r int, g int, b int) uint32 {
+	return uint32(uint32(r)<<16 | uint32(g)<<8 | uint32(b))
+}
+
+func (ctrl *Controller) Render(drv *ws281x.WS2811, cats map[int]metar.FlightCategory) error {
+	leds := drv.Leds(0)
+	for i := 0; i < len(leds); i++ {
+		cat := cats[i]
+		rgb := ctrl.Colors[cat]
+		leds[i] = rgb.ToColor()
+	}
+
+	if err := drv.Render(); err != nil {
 		return err
+	}
+
+	if err := drv.Wait(); err != nil {
+		if l := ctrl.Logger; l != nil {
+			l.Warn("wait failure", "error", err)
+		}
 	}
 
 	return nil
 }
 
-func (ws *ws) close() {
-	ws.ws2811.Fini()
+func (ctrl *Controller) DefaultOptions() []Option {
+	return []Option{
+		func(opt *ws281x.ChannelOption) {
+			opt.Brightness = DefaultBrightness
+			opt.LedCount = DefaultLEDCounts
+			opt.GpioPin = DefaultGPIOPin
+		},
+	}
 }
 
-func rgbToColor(r int, g int, b int) uint32 {
-	return uint32(uint32(r)<<16 | uint32(g)<<8 | uint32(b))
+func (ctrl *Controller) applyOptions(drvopt *ws281x.Option, opts ...Option) {
+	for _, opt := range opts {
+		if opt == nil {
+			continue
+		}
+		chopt := drvopt.Channels[0]
+		opt(&chopt)
+		drvopt.Channels[0] = chopt
+	}
 }
 
-func (ws *ws) randomRGB() {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
+func (ctrl *Controller) Serve(ctx context.Context, rnd chan (map[int]metar.FlightCategory), opts ...Option) error {
+
+	drvopts := ws281x.DefaultOptions
+	ctrl.applyOptions(&drvopts, ctrl.DefaultOptions()...)
+	ctrl.applyOptions(&drvopts, opts...)
+
+	drv, err := ws281x.MakeWS2811(&drvopts)
+	if err != nil {
+		return fmt.Errorf("failed to create driver: %w", err)
+	}
+
+	if err := drv.Init(); err != nil {
+		return fmt.Errorf("failed to initialize: %w", err)
+	}
+
+	defer drv.Fini()
 
 	for {
-		for i := 0; i < len(ws.ws2811.Leds(0)); i++ {
-			ws.ws2811.Leds(0)[i] = rgbToColor(r1.Intn(255), r1.Intn(255), r1.Intn(255))
+		select {
+		case cats := <-rnd:
+			if err := ctrl.Render(drv, cats); err != nil {
+				if l := ctrl.Logger; l != nil {
+					l.Error("render failure", "error", err)
+				}
+			}
+		case <-ctx.Done():
+			if l := ctrl.Logger; l != nil {
+				l.Debug("context done", "error", ctx.Err())
+			}
+			return nil
 		}
-
-		ws.ws2811.Render()
-
-		time.Sleep(sleepTime * time.Millisecond)
 	}
-}
-
-func main() {
-	opt := ws281x.DefaultOptions
-	opt.Channels[0].Brightness = brightness
-	opt.Channels[0].LedCount = ledCounts
-	opt.Channels[0].GpioPin = gpioPin
-	opt.Frequency = freq
-
-	ws2811, err := ws281x.MakeWS2811(&opt)
-	if err != nil {
-		panic(err)
-	}
-
-	ws := ws{
-		ws2811: ws2811,
-	}
-
-	err = ws.init()
-	if err != nil {
-		panic(err)
-	}
-	defer ws.close()
-
-	ws.randomRGB()
 }
